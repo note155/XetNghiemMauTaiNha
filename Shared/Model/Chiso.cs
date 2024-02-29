a3813dc865ab5e2ae95eeca0972f971435a7ef9a
Windows 10 <Windows 10@21AK22-COM>
2024-02-29 09:18:00 +0700

    29/2/2024

---
+++ Shared/Model/Chiso.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Model
+{
+    public class Chiso
+    {
+        [Key]
+        public int Id { get; set; }
+        
+        public string ten {  get; set; }
+        public string name { get; set; }
+
+    }
+}
